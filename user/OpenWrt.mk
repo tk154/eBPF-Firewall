337b7a566b40347fa23454e697cc6449ec52aec4
Til Kaiser <mail@tk154.de>
2024-03-17 16:43:42 +0100

    Adjust wildcard matching
    Signed-off-by: Til Kaiser <mail@tk154.de>

--- user/OpenWrt.mk
+++ user/OpenWrt.mk
@@ -2,14 +2,18 @@ ifndef TARGET
 $(error Missing TARGET)
 endif
 
+define first_wildcard_match
+	$(shell set -- $(1); echo "$$1")
+endef
+
 
 GCC_VERSION := 12.3.0
 
-TOOLCHAIN_DIR := $(shell set -- $(OPENWRT_DIR)/staging_dir/toolchain-$(TARGET)*_gcc-$(GCC_VERSION)_musl; echo "$$1")
-TARGET_DIR    := $(shell set -- $(OPENWRT_DIR)/staging_dir/target-$(TARGET)*_musl; echo "$$1")
+TOOLCHAIN_DIR := $(call first_wildcard_match,$(OPENWRT_DIR)/staging_dir/toolchain-$(TARGET)*_gcc-$(GCC_VERSION)_musl*)
+TARGET_DIR    := $(call first_wildcard_match,$(OPENWRT_DIR)/staging_dir/target-$(TARGET)*_musl*)
 
-CC  := $(TOOLCHAIN_DIR)/bin/$(TARGET)-openwrt-linux-musl-gcc
-CXX := $(TOOLCHAIN_DIR)/bin/$(TARGET)-openwrt-linux-musl-g++
+CC  := $(call first_wildcard_match,$(TOOLCHAIN_DIR)/bin/$(TARGET)-openwrt-linux-musl*-gcc)
+CXX := $(call first_wildcard_match,$(TOOLCHAIN_DIR)/bin/$(TARGET)-openwrt-linux-musl*-g++)
 
 CPPFLAGS += -I$(TARGET_DIR)/usr/include
 LDFLAGS  += -L$(TARGET_DIR)/usr/lib
