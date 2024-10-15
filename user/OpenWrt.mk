2f0b2b131057e7dcea8d45826265862a83b8dfea
Til Kaiser <mail@tk154.de>
2024-10-15 21:26:14 +0200

    Use the latest GCC version
    Signed-off-by: Til Kaiser <mail@tk154.de>

--- user/OpenWrt.mk
+++ user/OpenWrt.mk
@@ -1,13 +1,15 @@
 ARCH ?= $(shell uname -m)
-GCC  ?= 14.1.0
 
-TOOLCHAIN_DIR := $(wildcard $(OPENWRT_DIR)/staging_dir/toolchain-$(ARCH)_*gcc-$(GCC)_musl*)
-TARGET_DIR    := $(wildcard $(OPENWRT_DIR)/staging_dir/target-$(ARCH)_*musl*)
+TOOLCHAIN_DIR := $(lastword $(wildcard $(OPENWRT_DIR)/staging_dir/toolchain-$(ARCH)_*gcc-*_musl*))
+TARGET_DIR    :=            $(wildcard $(OPENWRT_DIR)/staging_dir/target-$(ARCH)_*musl*)
+
+ifndef TOOLCHAIN_DIR
+$(error "No toolchain directory found in $(OPENWRT_DIR)/staging_dir for arch $(ARCH)")
+endif
+
+CC := $(notdir $(wildcard $(TOOLCHAIN_DIR)/bin/$(ARCH)-openwrt-linux-musl*-gcc))
+CFLAGS 	+= -I$(TARGET_DIR)/usr/include
+LDFLAGS += -L$(TARGET_DIR)/usr/lib
 
 export PATH 	   := $(PATH):$(TOOLCHAIN_DIR)/bin
 export STAGING_DIR := $(TOOLCHAIN_DIR):$(TARGET_DIR)/usr
-
-CC := $(notdir $(wildcard $(TOOLCHAIN_DIR)/bin/$(ARCH)-openwrt-linux-musl*-gcc))
-
-CFLAGS 	+= -I$(TARGET_DIR)/usr/include
-LDFLAGS += -L$(TARGET_DIR)/usr/lib
